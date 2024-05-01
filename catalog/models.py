59724a1e245cec0398b3bef96b430b6485f25d78
Leonid Petrov <leonidpetrov637@gmail.com>
2024-05-01 20:39:31 +0300

    Created Category and Product models and described the initial settings

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['Category']
