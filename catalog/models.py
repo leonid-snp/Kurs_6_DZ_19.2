a5e580302df819aeec47c5821e40be04acf26923
Leonid Petrov <leonidpetrov637@gmail.com>
2024-05-01 22:06:39 +0300

    Created migrations for new models

--- catalog/models.py
+++ catalog/models.py
@@ -1,34 +1,82 @@
 from django.db import models
 
 
-NULLABLE = {'blank': True, 'null': True}
+NULLABLE = {"blank": True, "null": True}
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название категории', help_text='Введите название категории')
-    description = models.TextField(verbose_name='Описание категории', help_text='Введите описание категории', **NULLABLE)
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Название категории",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        **NULLABLE
+    )
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название продукта', help_text='Введите название продукта')
-    description = models.TextField(verbose_name='Описание продукта', help_text='Введите описание продукта', **NULLABLE)
-    photo = models.ImageField(upload_to='products/', verbose_name='Фото продукта', help_text='Загрузите фото продукта', **NULLABLE)
-    category = models.ForeignKey(on_delete=models.SET_NULL, verbose_name='Категория', help_text='Введите название категории', **NULLABLE)
-    price = models.IntegerField(verbose_name='Цена продукта', help_text='Введите цену продукта', **NULLABLE)
-    created_at = models.DateField(verbose_name='Дата создания', help_text='Укажите дату создания', **NULLABLE)
-    updated_at = models.DateField(verbose_name='Дата изменения', help_text='Укажите дату изменения', **NULLABLE)
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Название продукта",
+        help_text="Введите название продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        **NULLABLE
+    )
+    photo = models.ImageField(
+        upload_to="products/",
+        verbose_name="Фото продукта",
+        help_text="Загрузите фото продукта",
+        **NULLABLE
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите название категории",
+        related_name="Products",
+        **NULLABLE
+    )
+    price = models.IntegerField(
+        verbose_name="Цена продукта",
+        help_text="Введите цену продукта",
+        **NULLABLE
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания",
+        help_text="Укажите дату создания",
+        **NULLABLE
+    )
+    updated_at = models.DateField(
+        verbose_name="Дата изменения",
+        help_text="Укажите дату изменения",
+        **NULLABLE
+    )
+    manufactured_at = models.DateField(
+        verbose_name="Дата производства продукта",
+        help_text="Укажите дату производства продукта",
+        **NULLABLE
+    )
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['Category']
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = [
+            "category", "name", "price",
+            "created_at", "updated_at"
+        ]
