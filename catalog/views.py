9ef48430237dfff8d4b4948df9352aa06fb781df
Leonid Petrov <leonidpetrov637@gmail.com>
2024-05-07 23:21:11 +0300

    I navigated through the pages and started refining the form filling

--- catalog/views.py
+++ catalog/views.py
@@ -4,13 +4,9 @@ from catalog.models import Product, Contact, Category
 
 
 def home(request):
-    product_list = Product.objects.all()
-    latest_five_products = product_list[len(product_list) - 5 :]
     context = {
-        "object_list": latest_five_products,
         "title": 'Главная страница'
     }
-    print(latest_five_products)
     return render(request, "catalog/home.html", context)
 
 
@@ -53,16 +49,18 @@ def add_product(request):
         created_at = request.POST.get("created_at")
         updated_at = request.POST.get("updated_at")
 
-        Product.objects.create(
-            name=name,
-            description=description,
-            photo=photo,
-            category=category,
-            price=price,
-            created_at=created_at,
-            updated_at=updated_at
-        )
+        # Product.objects.create(
+        #     name=name,
+        #     description=description,
+        #     photo=photo,
+        #     category=category,
+        #     price=price,
+        #     created_at=created_at,
+        #     updated_at=updated_at
+        # )
+        print(request.POST)
         print(name, description, photo, category, price, created_at, updated_at)
+        print(category_list[0])
     context = {
         'object_list': category_list,
         'title': 'Добавить продукт'
@@ -70,9 +68,20 @@ def add_product(request):
     return render(request, 'catalog/add_product.html', context)
 
 
+def home_1(request):
+    product_list = Product.objects.all()
+    latest_five_products = product_list[:4]
+    context = {
+        "object_list": latest_five_products,
+        "title": 'Вторая страница'
+    }
+    print(latest_five_products)
+    return render(request, "catalog/home_2.html", context)
+
+
 def home_2(request):
     product_list = Product.objects.all()
-    latest_five_products = product_list[len(product_list) - 5 :]
+    latest_five_products = product_list[4:]
     context = {
         "object_list": latest_five_products,
         "title": 'Вторая страница'
